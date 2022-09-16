9bc9f4c8ee81716db497b67d62b74409e46d7833
Mikhail Popov <mike.popov.nj@gmail.com>
2022-09-16 23:21:11 +0300

    G-0 Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="gt">
+    <header>
+
+    </header>
+
+    <div class="gt-content">
+      <main>
+
+      </main>
+
+      <aside>
+
+      </aside>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss"></style>
